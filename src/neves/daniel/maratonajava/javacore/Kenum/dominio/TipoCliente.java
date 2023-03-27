a5132ae42e86fcac7ee84da31e30e64dfc8d18f4
Daniel Neves <danielnevesdecastro@gmail.com>
2023-03-27 14:33:59 -0300

    Enum

---
+++ src/neves/daniel/maratonajava/javacore/Kenum/dominio/TipoCliente.java
@@ -0,0 +1,5 @@
+package neves.daniel.maratonajava.javacore.Kenum.dominio;
+//80 - enum
+public enum TipoCliente {
+    PESSOA_FISICA, PESSOA_JURIDICA;
+}
---
+++ src/neves/daniel/maratonajava/javacore/Kenum/test/ClienteTest01.java
@@ -0,0 +1,23 @@
+package neves.daniel.maratonajava.javacore.Kenum.test;
+
+import neves.daniel.maratonajava.javacore.Kenum.dominio.Cliente;
+import neves.daniel.maratonajava.javacore.Kenum.dominio.TipoCliente;
+
+//80 - enum
+public class ClienteTest01 {
+
+    public static void main(String[] args) {
+        Cliente c1 = new Cliente("Daniel", TipoCliente.PESSOA_FISICA);
+        Cliente c2 = new Cliente("Neves", TipoCliente.PESSOA_JURIDICA);
+        Cliente c3 = new Cliente("William", TipoCliente.PESSOA_FISICA);
+        Cliente c4 = new Cliente("Suane", TipoCliente.PESSOA_JURIDICA);
+        Cliente[] clientes = {c1, c2, c3, c4};
+
+        for (Cliente cliente : clientes) {
+
+            System.out.println(cliente);
+
+        }
+
+    }
+}
