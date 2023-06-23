1313d01b3eb0a78bce3f423dbc946f86024ead2e
Daniel Neves <danielnevesdecastro@gmail.com>
2023-06-22 22:51:33 -0300

    Exception

---
+++ src/neves/daniel/maratonajava/javacore/Oexception/test/RuntimeExceptionTest01.java
@@ -0,0 +1,14 @@
+package neves.daniel.maratonajava.javacore.Oexception.test;
+
+public class RuntimeExceptionTest01 {
+    public static void main(String[] args) {
+        //Checked Unchecked
+
+        //Runtimeexception - unchecked
+        //Object object = null;
+        //System.out.println(object.toString());
+        //int[] nums = {1,2};
+        //System.out.println(nums[2]);
+    }
+
+}
